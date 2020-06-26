5a7676f6e5428f81ba84a253c43893ed73e321fc
Mckenz31 <mckenz318@gmail.com>
2020-06-26 18:15:18 +0530

    first commit: The basics - intro+component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //template
+  styleUrls: ['./app.component.css'] ,
+
+  //This is styling using an inline template
+  /*styles: [`
+  h3{
+    color: pink;
+  }
+  `] */
+
 })
 export class AppComponent {
-  title = 'my-first-app';
 }
