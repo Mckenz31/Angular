b99d8266d1122e76690b38bf76eb8aa2849f4123
Mckenz31 <mckenz318@gmail.com>
2020-07-01 12:12:23 +0530

    Binding Custom Properties & Custom Events and Assigning an Alias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  //template
-  styleUrls: ['./app.component.css'] ,
-
-  //This is styling using an inline template
-  /*styles: [`
-  h3{
-    color: pink;
-  }
-  `] */
-
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  serverElements = [];
+
+  onServerAdded(serverData: {serverName: string, serverContent:string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+
+  onBlueprintAdded(serverData: {serverName: string, serverContent:string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
-import { ServersComponent } from './servers/servers.component';
-import { ServerComponent } from './server/server.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServersComponent,
-    ServerComponent
+    CockpitComponent,
+    ServerElementComponent,
+
   ],
   imports: [
     BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
