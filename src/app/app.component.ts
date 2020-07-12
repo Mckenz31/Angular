6c5f7f93009cc9af3d1812c52a9aadd5fbcc9bba
Mckenz31 <mckenz318@gmail.com>
2020-07-12 17:50:06 +0530

    Routing setup code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  //template
-  styleUrls: ['./app.component.css'] ,
-
-  //This is styling using an inline template
-  /*styles: [`
-  h3{
-    color: pink;
-  }
-  `] */
-
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 }
