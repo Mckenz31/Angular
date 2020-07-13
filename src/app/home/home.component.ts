21ae8e13b079596d389f02fab2b4bdfb84d723ad
Mckenz31 <mckenz318@gmail.com>
2020-07-13 20:42:32 +0530

    Protecting Child Routes & created fake auth
    - protecting childroutes using canActivateChild
    - created logIn / logOut options by fake auth

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,8 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private route: Router, private activeRoute: ActivatedRoute) { }
+  constructor(private route: Router, private activeRoute: ActivatedRoute,
+    private authServ: AuthService) { }
 
   ngOnInit() {
   }
@@ -16,6 +18,13 @@ export class HomeComponent implements OnInit {
   onClick(){
     //complicated code
     this.route.navigate(['/servers','5','edit'], {queryParams: {homeToservers: 77}, fragment: 'whocares'});
+  }
+
+  onLogin(){
+    this.authServ.logIn();
+  }
+  onLogout(){
+    this.authServ.logOut();
   }
 
 }
