f4c1178f6411b12edcc2de89691be39e76069f49
Mckenz31 <mckenz318@gmail.com>
2020-07-21 23:22:42 +0530

    Building my first custom observable -const observ = Observable.create(observer => {
    })
    Observ.subscribe();

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { interval, Subscription, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -13,8 +13,19 @@ export class HomeComponent implements OnInit, OnDestroy {
   constructor() { }
 
   ngOnInit() {
-    this.sub = interval(1000).subscribe(data=>{
-      console.log(data);
+    // this.sub = interval(1000).subscribe(data=>{
+    //   console.log(data);
+    // })
+    const Observ = Observable.create(observer => {
+      let count = 0;
+      setInterval(()=> {
+        observer.next(count);
+        count++
+      }, 1000)
+    } )
+
+    this.sub = Observ.subscribe(dataz => {
+      console.log(dataz);
     })
   }
 
