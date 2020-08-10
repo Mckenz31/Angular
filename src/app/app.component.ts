1ede235e81512a814095688fc58a6e97911baaca
Mckenz31 <mckenz318@gmail.com>
2020-08-10 22:29:03 +0530

    Reactive Form
    - syncing html and form, submit, adding validation
    - FormGroup
    - FormControl(ts) FormControlName(html)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,51 +1,25 @@
-import { Component, ViewChild} from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  genders = ['male', 'female'];
+  reactivFrm: FormGroup;
 
-  @ViewChild('f') duh:NgForm
-  defopt = 'pet';
-  textdetail = '';
-  genders: any = ['male', 'female'];
-  condition = false;
-  userData:any = {userName: '',
-    emailID: '',
-    confidential: '',
-    inputData: '',
-    sex: '',}
-
-  suggestUserName() {
-
-    // this.duh.setValue({
-    //   userData: {
-    //     user: 'Superuser',
-    //     mail: 'rosita@hotmail.com'
-    //   },
-    //   secret: 'teacher',
-    //   textContent: 'Rosita',
-    //   gender: 'female'
-    // });
-
-    this.duh.form.patchValue({
-      userData: {
-        user: 'PatchUser'
-      }
-    })
+  ngOnInit(){
+    this.reactivFrm = new FormGroup({
+      'username': new FormControl(null, Validators.required),
+      'mail': new FormControl(null, [Validators.required , Validators.email]),
+      'radio': new FormControl('male', Validators.required)
+    });
   }
 
   onSubmit(){
-    console.log(this.duh);
-    this.userData.userName = this.duh.value.userData.user,
-    this.userData.emailID = this.duh.value.userData.mail,
-    this.userData.confidential = this.duh.value.secret,
-    this.userData.inputData = this.duh.value.textContent,
-    this.userData.sex = this.duh.value.gender,
-    this.condition = true;
-    this.duh.reset();
+    console.log(this.reactivFrm);
   }
+
 }
