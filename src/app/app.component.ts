5d1deadff942f539a45fe92d9d190b70fe04ce6b
Mckenz31 <mckenz318@gmail.com>
2020-08-11 18:20:47 +0530

    Reactive Forms - Array of Form Controls
    - 'xyz': new FormArray([])
    - In ts, we will use FormControl and <FormArray>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -16,12 +16,23 @@ export class AppComponent implements OnInit{
         'username': new FormControl(null, Validators.required),
       'mail': new FormControl(null, [Validators.required , Validators.email]),
       }),
-      'radio': new FormControl('male', Validators.required)
+      'radio': new FormControl('male', Validators.required),
+      'hobbies': new FormArray([])
     });
   }
 
   onSubmit(){
     console.log(this.reactivFrm);
+  }
+
+  onAddHobby(){
+    const controls = new FormControl(null, Validators.required);
+    (<FormArray>this.reactivFrm.get('hobbies')).push(controls);
+  }
+
+  get controls(){
+    return (<FormArray>this.reactivFrm.get('hobbies')).controls;
+    // return (this.reactivFrm.get('hobbies') as FormArray).controls; -- SAME AS THE PREVIOUS LINE OF CODE
   }
 
 }
