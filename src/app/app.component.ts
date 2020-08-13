64bb9b29bf890273c41239e8491a9a0d66fa4230
Mckenz31 <mckenz318@gmail.com>
2020-08-13 17:53:01 +0530

    Pipes -initial code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,40 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  //template
-  styleUrls: ['./app.component.css'] ,
-
-  //This is styling using an inline template
-  /*styles: [`
-  h3{
-    color: pink;
-  }
-  `] */
-
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  servers = [
+    {
+      instanceType: 'medium',
+      name: 'Production Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'large',
+      name: 'User Database',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Development Server',
+      status: 'offline',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Testing Environment Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    }
+  ];
+  getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
+    return {
+      'list-group-item-success': server.status === 'stable',
+      'list-group-item-warning': server.status === 'offline',
+      'list-group-item-danger': server.status === 'critical'
+    };
+  }
 }
