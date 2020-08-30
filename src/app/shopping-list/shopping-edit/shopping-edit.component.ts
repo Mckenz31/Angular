11794fbf38c65832626174c7c66429ba27817a40
Mckenz31 <mckenz318@gmail.com>
2020-08-30 12:56:15 +0530

    CP - Adding the shopping list form and validators
    - We are using the template driven approach
    - When entering the amount, even if we enter -2, the form considers it valid but that should be invalid in our case
    Hence we use - pattern="^[1-9]+[0-9]*$".
    We can also use - [pattern]="'^[1-9]+[0-9]*$'"

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -9,21 +10,15 @@ import { ShoppingListService } from '../shopping-list.service';
 })
 export class ShoppingEditComponent implements OnInit {
 
-  @ViewChild('nameShop',{static: false}) nameRef: ElementRef;
-  @ViewChild('amountShop', {static: false}) amountRef: ElementRef;
-
   constructor(private shopServ: ShoppingListService) { }
 
   ngOnInit(): void {
   }
 
-  addButton(){
-    const namez = this.nameRef.nativeElement.value;
-    const amountz = this.amountRef.nativeElement.value;
-    const ingData = new Ingredient(namez, amountz);
+  onSubmit(form:NgForm){
+    const data = form.value
+    const ingData = new Ingredient(data.username, data.amount);
     this.shopServ.onEnterIng(ingData);
-    // this.shopDataz.emit(ingData);
   }
-
 
 }
